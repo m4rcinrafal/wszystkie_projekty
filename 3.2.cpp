304669f82017eea22336bec5c3e05f0ea7fba157
Marcin Rafal <mr322238@student.polsl.pl>
2025-10-28 15:03:41 +0100

    28.10

---
+++ 3.2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	string imie; int calkowita;
+	cout << "Podaj imie" << endl;
+	cin >> imie;
+	cout << "Podaj liczbe calkowita" << endl;
+	cin >> calkowita;
+	
+	for (int i = 1;i <= calkowita;i++)
+	{
+		cout << i << ". " << imie<<endl;
+	}
+}
